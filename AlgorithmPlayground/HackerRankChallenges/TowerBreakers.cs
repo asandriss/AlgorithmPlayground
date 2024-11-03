7fdc4bdd9925b480c7349394774400bc6f6f4415
Fedor Hajdu <fedorhajdu@gmail.com>
2024-11-03 13:07:33 +0000

    Tower Breakers solution

---
+++ AlgorithmPlayground/HackerRankChallenges/TowerBreakers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground.HackerRankChallenges
+{
+    public class TowerBreakers
+    {
+        public static int PlayTowerBreakers(int numberOfTowers, int towerStartingHeight)
+        {
+            if(towerStartingHeight == 1 || numberOfTowers % 2 == 0)
+                return 2;
+
+            return 1;
+
+        }
+    }
+}
---
+++ AlgorithmPlaygroundTests/HackerRankChallenges/TowerBreakersTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmPlayground.HackerRankChallenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground.HackerRankChallenges.Tests
+{
+    [TestClass()]
+    public class TowerBreakersTests
+    {
+        [TestMethod()]
+        public void SimplestInput_PlayerTwoWins()
+        {
+            int actual = TowerBreakers.PlayTowerBreakers(1, 4);
+            int expect = 1;
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void ChallengeInput_PlayerTwoWins()
+        {
+            int actual = TowerBreakers.PlayTowerBreakers(2, 6);
+            int expect = 2;
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
