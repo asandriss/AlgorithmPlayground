f968f82693727a35e9c3e7e408d56e997b10d41f
Fedor Hajdu <fedorhajdu@gmail.com>
2024-11-05 21:59:24 +0000

    Move Zeroes solution - quadratic complexity, should be possible to improve.

---
+++ AlgorithmPlayground/LeetCode/MoveZeroes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground.LeetCode
+{
+    public class MoveZeroes
+    {
+        public static void MoveZeroesToTheEnd(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1) return;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    // we are at zero, need to swap
+                    //  find the first not-zero index
+                    var candidate = i + 1;
+                    while (candidate < nums.Length)
+                    {
+                        if (nums[candidate] != 0)
+                        {
+                            //var tmp = nums[i];
+                            nums[i] = nums[candidate];
+                            nums[candidate] = 0;
+                            break;      // found a swap, we can exit the inner loop
+                        }
+
+                        candidate++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmPlaygroundTests/LeetCode/MoveZeroesTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmPlayground.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace AlgorithmPlayground.LeetCode.Tests
+{
+    [TestClass()]
+    public class MoveZeroesTests
+    {
+        [TestMethod()]
+        public void TestEmptyArray_ShouldNotAffectTheInput()
+        {
+            var input1actual = new int[] { };
+            int[] input2actual = null;
+            var input3actual = new int[] { 55 };
+
+            var expect1 = Array.Empty<int>();
+            int[] expect2 = null;
+            int[] expect3 = new int[] { 55 };
+
+            MoveZeroes.MoveZeroesToTheEnd(input1actual);
+            MoveZeroes.MoveZeroesToTheEnd(input2actual);
+
+            input1actual.Should().Equal(expect1);
+            input2actual.Should().Equal(expect2);
+            input3actual.Should().Equal(expect3);
+        }
+
+        [TestMethod()]
+        public void TwoElementsWithLeadingZero_Should_MoveZeroBack()
+        {
+            var input1actual = new int[] { 0, 1 };
+            var expect = new int[] { 1, 0 };
+
+            MoveZeroes.MoveZeroesToTheEnd(input1actual);
+
+            input1actual.Should().Equal(expect);
+        }
+
+        [TestMethod()]
+        public void ManyNonconsecutiveZeros_Should_MoveZeroBack()
+        {
+            var input1actual = new int[] { 0, 1, 2, 0, 3, 0, 4 };
+            var expect = new int[] { 1, 2, 3, 4, 0, 0, 0 };
+
+            MoveZeroes.MoveZeroesToTheEnd(input1actual);
+
+            input1actual.Should().Equal(expect);
+        }
+    }
+}
