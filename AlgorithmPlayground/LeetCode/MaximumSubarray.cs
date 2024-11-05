eb166a9eef61c5ef0e3e8dc90bb71fd8c01e6bd4
Fedor Hajdu <fedorhajdu@gmail.com>
2024-11-05 20:57:54 +0000

    Add MaximumSubarray challenge

---
+++ AlgorithmPlayground/LeetCode/MaximumSubarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground.LeetCode
+{
+    public class MaximumSubarray
+    {
+        public static int GetMax(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+
+            long current = int.MinValue;
+            long max = int.MinValue;
+
+            foreach (var t in nums)
+            {
+                current = Math.Max(t, current + t);
+                max = Math.Max(max, current);
+            }
+
+            return (int)max;
+        }
+    }
+}
---
+++ AlgorithmPlaygroundTests/LeetCode/MaximumSubarrayTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmPlayground.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace AlgorithmPlayground.LeetCode.Tests
+{
+    [TestClass()]
+    public class MaximumSubarrayTests
+    {
+        [TestMethod()]
+        public void EmptyArray_ShouldReturn_Zero()
+        {
+            var expect = 0;
+            var input = new int[0];
+
+            var actual = MaximumSubarray.GetMax(input);
+
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void SingleElementInput_ShouldReturn_ElementAtZero()
+        {
+            var expect = 55;
+            var input = new int[] { 55 };
+
+            var actual = MaximumSubarray.GetMax(input);
+
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void TwoPositiveValues_ShouldReturn_Sums()
+        {
+            var expect = 69;
+            var input = new int[] { 55, 14 };
+
+            var actual = MaximumSubarray.GetMax(input);
+
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void ProvidedInput_ShouldReturn_MaxSubarray()
+        {
+            var expect = 6;
+            var input = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+            var actual = MaximumSubarray.GetMax(input);
+
+            actual.Should().Be(expect);
+        }
+
+    }
+}
