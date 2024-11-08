e6f8baf427555bdb25cf77bf3e6db692c3223c29
Fedor Hajdu <fedorhajdu@gmail.com>
2024-11-08 23:08:28 +0000

    LeetCode MergeStringsAlternately (1768) first attempt

---
+++ AlgorithmPlayground/LeetCode/MergeStringsAlternately.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPlayground.LeetCode
+{
+    public class MergeStringsAlternately
+    {
+        public static string Merge(string word1, string word2)
+        {
+            var arr1 = word1.ToCharArray();
+            var arr2 = word2.ToCharArray();
+            var commonLength = Math.Min(arr1.Length, arr2.Length);
+            
+            var result = string.Empty;
+
+            for (int i = 0; i < commonLength; i++)
+            {
+                result += arr1[i].ToString() + arr2[i].ToString();
+            }
+
+            if (arr1.Length > commonLength)
+                result += word1.Substring(commonLength);
+
+            if (arr2.Length > commonLength)
+                result += word2.Substring(commonLength);
+            
+            return result;
+        }
+    }
+}
---
+++ AlgorithmPlaygroundTests/LeetCode/MergeStringsAlternatelyTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmPlayground.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace AlgorithmPlayground.LeetCode.Tests
+{
+    [TestClass]
+    public class MergeStringsAlternatelyTests
+    {
+        [TestMethod]
+        public void MergeTest()
+        {
+            var word1 = "ab";
+            var word2 = "pqrs";
+            var expect = "apbqrs";
+
+            var actual = MergeStringsAlternately.Merge(word1, word2);
+
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod]
+        public void MergeTest_FourTwo()
+        {
+            var word1 = "abcd";
+            var word2 = "pq";
+            var expect = "apbqcd";
+
+            var actual = MergeStringsAlternately.Merge(word1, word2);
+
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod]
+        public void Merge_TestSingleCharInput()
+        {
+            var word1 = "cdf";
+            var word2 = "a";
+            var expect = "cadf";
+
+            var actual = MergeStringsAlternately.Merge(word1, word2);
+
+            actual.Should().Be(expect);
+
+        }
+    }
+}
