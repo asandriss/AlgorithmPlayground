417a8670a24adfd3923080b7a1bfd797ecd61b24
Fedor Hajdu <fedorhajdu@gmail.com>
2024-12-01 02:33:27 +0000

    Simple currying demo

--- AlgorithmPlayground/Program.cs
+++ AlgorithmPlayground/Program.cs
@@ -10,6 +10,18 @@ namespace AlgorithmPlayground
     {
         static void Main(string[] args)
         {
+            // init basic repo info
+            var getGithubRepo = GetUrl("https://github.com/");
+            
+            // ... later
+            var myRepo = getGithubRepo("asandriss/AlgorithmPlayground");
+            
+            Console.WriteLine(myRepo);
+        }
+
+        static Func<string, string> GetUrl(string baseHost)
+        {
+            return x => $"{baseHost}/{x}";
         }
     }
 }
